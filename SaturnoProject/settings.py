b84494d18cd77562b13c9b209021221c69bbd633
yaszcdp <yasz.utn@hotmail.com>
2024-09-03 21:44:58 -0300

    configurando para pythonanywhere

--- SaturnoProject/settings.py
+++ SaturnoProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-97qgzy^whdts@&7e-^(e9jw!)2=w1et+glar$po$vm8+5d4ird'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["echidnamun.pythonanywhere.com"]
 
 
 # Application definition
